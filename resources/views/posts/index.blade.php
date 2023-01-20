a05e000cb993bd35af1c7f147c9f211a09f40118
TajipinAcid <tajimakosuke1614hr@icloud.com>
2023-01-21 01:17:55 +0900

    ペジネーション動作確認

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        
         <meta charset="utf-8">
         <title>Blog</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     
@@ -17,6 +17,9 @@
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
